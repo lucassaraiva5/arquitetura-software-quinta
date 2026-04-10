6e48e1d96fa9c8328ad298c6b7826bde32238b41
Lucas Saraiva <lucassaraiva5@hotmail.com>
2026-04-09 21:57:44 -0300

    Minha mensagem do commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 import domain.Product;
 import service.ProductService;
+import utils.GenerateValue;
 
 void main() {
     ProductService productService = new ProductService();
 
-    productService.create(new Product(
-                    productService.generateUUID(),
-                    "SKU",
-                    "asas",
-                    new BigDecimal(2))
-    );
+    Product produto = new Product(
+            GenerateValue.uuid(),
+            "SKU",
+            "asas",
+            2f);
+    produto.setPrice(3f);
+    produto.setPrice(4f);
+    productService.create(produto);
 
     productService.listAll();
 
--- src/domain/Price.java
+++ src/domain/Price.java
@@ -6,18 +6,18 @@ import java.util.UUID;
 
 public class Price implements EntityInterface {
     private UUID uuid;
-    private BigDecimal price;
+    private Float price;
     private Date date;
 
     public Price() {
     }
 
-    public Price(BigDecimal price, Date date) {
+    public Price(Float price, Date date) {
         this.price = price;
         this.date = date;
     }
 
-    public Price(UUID uuid, BigDecimal price, Date date) {
+    public Price(UUID uuid, Float price, Date date) {
         this.uuid = uuid;
         this.price = price;
         this.date = date;
