863e078d81a94639ff2b1ffdd6437524f1953e4c
Lucas Saraiva <lucassaraiva5@hotmail.com>
2026-05-07 20:17:09 -0300

    Fix main method declaration and formatting in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 import domain.Product;
 import service.ProductService;
 
-void main() {
-    ProductService productService = new ProductService();
+public class Main {
+    public static void main(String[] args) {
+        ProductService productService = new ProductService();
 
-    Product produto = new Product("SKU", "asas", 2f);
-    produto.setPrice(3f);
-    produto.setPrice(4f);
-    productService.create(produto);
+        Product produto = new Product("SKU", "asas", 2f);
+        produto.setPrice(3f);
+        produto.setPrice(4f);
+        productService.create(produto);
 
-    productService.listAll();
+        productService.listAll();
+    }
 }
